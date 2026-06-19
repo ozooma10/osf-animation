42e4c8d9957a237509841743c0c3b2a6d1799442
ozooma10 <98544147+ozooma10@users.noreply.github.com>
2026-06-19 12:16:45 -0400

    gtfo radians. (drop some dead math code)

--- src/Util/Math.h
+++ src/Util/Math.h
@@ -5,22 +5,9 @@
 
 namespace OSF::Util
 {
+	// kPi is used by scene-math tests; kDegToRad/kDegToRadF convert authored degrees to runtime radians.
 	inline constexpr float  kPi  = std::numbers::pi_v<float>;
 	inline constexpr double kPiD = std::numbers::pi_v<double>;
 	inline constexpr double kDegToRad = kPiD / 180.0;  // pi/180
-	inline constexpr double kRadToDeg = 180.0 / kPiD;     // 180/pi
 	inline constexpr float kDegToRadF = static_cast<float>(kDegToRad);
-	inline constexpr float kRadToDegF = static_cast<float>(kRadToDeg);
-
-	// Wrap an angle (radians) into (-pi, pi].
-	inline float WrapRadians(float a_angle)
-	{
-		while (a_angle > kPi) {
-			a_angle -= 2.0f * kPi;
-		}
-		while (a_angle < -kPi) {
-			a_angle += 2.0f * kPi;
-		}
-		return a_angle;
-	}
 }
